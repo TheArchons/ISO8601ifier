48025f943435267513d6fef616898c748fe6e669
TheArchons <progamer9138@gmail.com>
2022-04-01 22:48:58 -0700

    pep 8 standardize

--- isoify.py
+++ isoify.py
@@ -1,16 +1,18 @@
 import sys
 
+
 class get():
     def getArgs():
         try:
-            if sys.argv[1] == '-h' or sys.argv[1] == '--help': #print help
+            if sys.argv[1] == '-h' or sys.argv[1] == '--help':  # print help
                 return '-h'
-            
-            #get date and time
+
+            # get date and time
             date = []
             try:
                 index = 1
-                while True: #get all arguments
+                # get all arguments
+                while True:
                     date.append(sys.argv[index])
                     index += 1
             except IndexError:
@@ -18,19 +20,23 @@ class get():
 
         except IndexError:
             return '-h'
+
     def printWelcome():
         print("welcome to isoify.py! Print -h for help.")
+
     def printHelp():
         print("isoify.py converts your time and date to ISO 8061 format.")
         print("-h or --help prints this message.")
         print("anything else will be interpreted as a time and date.")
 
+
 def main():
-    get.printWelcome() #prints welcome message
+    get.printWelcome()  # prints welcome message
     cmd = get.getArgs()
     if cmd == "-h" or cmd == "--help":
         get.printHelp()
     else:
-        get.getDate() #TODO make this a function
+        get.getDate()  # TODO make this a function
+
 
 main()
