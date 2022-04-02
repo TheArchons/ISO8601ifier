5ef5ee99b2ad033ee55864af86119205c5de6a26
TheArchons <progamer9138@gmail.com>
2022-04-02 16:53:19 -0700

    fix time and turn output into a dict

--- isoify.py
+++ isoify.py
@@ -80,7 +80,7 @@ class conversion():
         return ['date', int(s)]
     
     def time(s):
-        return ['time', int(s)]
+        return ['time', s]  # Do not convert to int because it may be colon seperated
 
 def Isoify(data):
     times = {
@@ -89,7 +89,7 @@ def Isoify(data):
         'timezones': {'acdt', 'acst', 'act', 'act', 'acwst', 'adt', 'aedt', 'aest', 'aet', 'aft', 'akdt', 'akst', 'almt', 'amst', 'amt', 'amt', 'anat', 'aqtt', 'art', 'ast', 'ast', 'awst', 'azost', 'azot', 'azt', 'bnt', 'biot', 'bit', 'bot', 'brst', 'brt', 'bst', 'bst', 'bst', 'btt', 'cat', 'cct', 'cdt', 'cdt', 'cest', 'cet', 'chadt', 'chast', 'chot', 'chost', 'chst', 'chut', 'cist', 'ckt', 'clst', 'clt', 'cost', 'cot', 'cst', 'cst', 'cst', 'ct', 'cvt', 'cwst', 'cxt', 'davt', 'ddut', 'dft', 'easst', 'east', 'eat', 'ect', 'ect', 'edt', 'eest', 'eet', 'egst', 'egt', 'est', 'et', 'fet', 'fjt', 'fkst', 'fkt', 'fnt', 'galt', 'gamt', 'get', 'gft', 'gilt', 'git', 'gmt', 'gst', 'gst', 'gyt', 'hdt', 'haec', 'hst', 'hkt', 'hmt', 'hovst', 'hovt', 'ict', 'idlw', 'idt', 'iot', 'irdt', 'irkt', 'irst', 'ist', 'ist', 'ist', 'jst', 'kalt', 'kgt', 'kost', 'krat', 'kst', 'lhst', 'lhst', 'lint', 'magt', 'mart', 'mawt', 'mdt', 'met', 'mest', 'mht', 'mist', 'mit', 'mmt', 'msk', 'mst', 'mst', 'mut', 'mvt', 'myt', 'nct', 'ndt', 'nft', 'novt', 'npt', 'nst', 'nt', 'nut', 'nzdt', 'nzst', 'omst', 'orat', 'pdt', 'pet', 'pett', 'pgt', 'phot', 'pht', 'phst', 'pkt', 'pmdt', 'pmst', 'pont', 'pst', 'pwt', 'pyst', 'pyt', 'ret', 'rott', 'sakt', 'samt', 'sast', 'sbt', 'sct', 'sdt', 'sgt', 'slst', 'sret', 'srt', 'sst', 'sst', 'syot', 'taht', 'tha', 'tft', 'tjt', 'tkt', 'tlt', 'tmt', 'trt', 'tot', 'tvt', 'ulast', 'ulat', 'utc', 'uyst', 'uyt', 'uzt', 'vet', 'vlat', 'volt', 'vost', 'vut', 'wakt', 'wast', 'wat', 'west', 'wet', 'wib', 'wit', 'wita', 'wgst', 'wgt', 'wst', 'yakt', 'yekt'}
     }
 
-    output = []
+    output = {}
     
     for s in data:
         try:
@@ -110,30 +110,45 @@ def Isoify(data):
         if type(s) == str:
             s = s.lower()
             if s in times['dates']:
-                output.append(conversion.date(s))
+                out = conversion.date(s)
+                output[out[0]] = out[1]
             elif s in times['days']:
-                output.append(conversion.day(s))
+                out = conversion.day(s)
+                output[out[0]] = out[1]
             elif s in times['timezones']:
-                output.append(conversion.timezone(s))
+                out = conversion.timezone(s)
+                output[out[0]] = out[1]
             elif 'UTC' in s:  # if it is a time relative to UTC
-                output.append(conversion.UTC(s))
+                out = conversion.UTC(s)
+                output[out[0]] = out[1]
+            elif ':' in s:
+                isTime = input(f'Is ${s} a time? (y/n)')
+                if isTime.lower() == 'y':
+                    out = conversion.time(s)
+                    output[out[0]] = out[1]
+
         elif type(s) == int:  # if it is a number
             if s > 31:
                 output.append(conversion.year(s))
             elif s > 24:
-                yearOrDate = input(f"Is ${str} a year or a date? (y/d) ")
+                yearOrDate = input(f"Is ${str} a year or a day? (y/d) ")
                 if yearOrDate == 'y':
-                    output.append(conversion.year(s))
+                    out = conversion.year(s)
+                    output[out[0]] = out[1]
                 elif yearOrDate == 'm':
-                    output.append(conversion.date(s))
+                    out = conversion.date(s)
+                    output[out[0]] = out[1]
             else:
-                yearOrDateOrTime = input(f"Is ${str} a year, a date, or a time? (y/d/t) ")
+                yearOrDateOrTime = input(f"Is ${str} a year, a day, or a time? (y/d/t) ")
                 if yearOrDateOrTime == 'y':
-                    output.append(conversion.year(s))
+                    out = conversion.year(s)
+                    output[out[0]] = out[1]
                 elif yearOrDateOrTime == 'd':
-                    output.append(conversion.date(s))
+                    out = conversion.date(s)
+                    output[out[0]] = out[1]
                 elif yearOrDateOrTime == 't':
-                    output.append(conversion.time(s))
+                    out = conversion.time(s)
+                    output[out[0]] = out[1]
 
     return output
             
